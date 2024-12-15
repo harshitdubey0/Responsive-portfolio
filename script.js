87c469c1826704b59b96b59a2a99ec38f08ecf68
Harshit Dubey <harshitdubey11578@gmail.com>
2024-12-15 21:00:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const list = document.getElementById('list');
+const bars = document.getElementById('bars');
+hamburger.addEventListener('click',()=>{
+    list.classList.toggle('navlist-active');
+    bars.classList.toggle('fa-xmark')
+})
